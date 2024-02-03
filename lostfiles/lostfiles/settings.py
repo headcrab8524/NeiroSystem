0af27fd300644ce7b5cba7ef8841607ecff5fcd8
unknown <zhukov561@gmail.com>
2024-02-03 20:22:24 +0300

    Конечная стадия создания БД

--- lostfiles/lostfiles/settings.py
+++ lostfiles/lostfiles/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xtnxwwwdk++=qf8a&egh+h8im*i=lfsh5ejpxt!h48x3twed9l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'lostfiles.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BD',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
@@ -122,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- lostfiles/main/models.py
+++ lostfiles/main/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to="photos/")
+    #photo = models.CharField(max_length=255)
+    login = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    role = models.ForeignKey('Role', on_delete=models.SET_NULL, null=True)
+    group = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+
+
+class ItemCard(models.Model):
+    name = models.CharField(max_length=255)
+    item_class = models.ForeignKey('Class', on_delete=models.SET_NULL, null=True)
+    comment = models.ForeignKey('Comment', on_delete=models.SET_NULL, null=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    resp_user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    status = models.BooleanField(default=True)
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    time_found = models.DateTimeField()
+    place_found = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to="photos/")
+    item_class = models.ForeignKey('Class', on_delete=models.SET_NULL, null=True)
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=255)
+    user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+
+
+class UserMark(models.Model):
+    user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    item_card = models.ForeignKey('ItemCard', on_delete=models.SET_NULL, null=True)
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Class(models.Model):
+    name = models.CharField(max_length=255)
