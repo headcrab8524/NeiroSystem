7336857d77dea807043a63d7cf645eccc3d6a9d9
unknown <zhukov561@gmail.com>
2024-02-10 20:14:36 +0300

    Настройка панели админа

--- lostfiles/main/admin.py
+++ lostfiles/main/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('login', 'role')
+    list_display_links = ()
+    search_fields = ('first_name', 'middle_name', 'last_name', 'login',
+                     'role', 'group', 'email')
+    list_editable = ()
+
+
+class ItemCardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'item_class', 'time_create', 'resp_user', 'status')
+    list_display_links = ()
+    search_fields = ('name', 'item_class', 'resp_user', 'status')
+    list_editable = ()
+    list_filter = ('item_class', 'resp_user', 'status')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(ItemCard, ItemCardAdmin)
--- lostfiles/main/models.py
+++ lostfiles/main/models.py
@@ -17,6 +17,11 @@ class User(models.Model):
     def __str__(self):
         return self.pk
 
+    class Meta:
+        verbose_name = 'Пользователи'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['id']
+
 
 class ItemCard(models.Model):
     name = models.CharField(max_length=255)
@@ -28,6 +33,11 @@ class ItemCard(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Карточки предметов'
+        verbose_name_plural = 'Карточки предметов'
+        ordering = ['time_create']
 
 
 class Item(models.Model):
