859d1d3e0d1beff8fdd4776b520e29dc177dc44d
Dmitry Rodin <rodindima2002@gmail.com>
2024-03-20 19:44:04 +0300

    Сформирован дизайн комментария

--- lostfiles/main/views.py
+++ lostfiles/main/views.py
@@ -51,7 +51,7 @@ class ShowCard(DataMixin, DetailView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title=context['card'].name)
-        context['comment'] = Comment.objects.filter = [context['card'].pk == 'item_card']
+        context['comment'] = Comment.objects.all() #TODO починить фильтрацию по предметам
         # context['title'] = context['card']
         return dict(list(context.items()) + list(c_def.items()))
 
